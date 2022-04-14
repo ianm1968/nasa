addb5df8c816987c7966ec7cdc3de9e6b8304e1c
ianm1968 <57989417+ianm1968@users.noreply.github.com>
2022-04-14 14:44:35 +0100

    Co-authored-by: HelgaVonTrapp <HelgaVonTrapp@users.noreply.github.com>

--- app/app.py
+++ app/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+import requests
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', landing_image='https://via.placeholder.com/500')
+    response=requests.get('https://api.nasa.gov/planetary/apod', params={'api_key':'ivjmWNKxDMBkaNoiXgaErQx3nm4A8ynl74fRaI0w'})
+    content=json.loads(response.content)
+    return render_template('index.html', landing_image=content['hdurl'])
 
 @app.route('/mars')
 def mars():
+    response=requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000', params={'api_key':'ivjmWNKxDMBkaNoiXgaErQx3nm4A8ynl74fRaI0w'})
+    content=json.loads(response.content)
     return render_template('mars.html')
